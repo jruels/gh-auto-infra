5cce4fdf7879ea94f2f7469512d807f31c4eb283
Jason Smith <jason@smithss.org>
2025-01-06 15:17:54 -0800

    use Account Factory creds

--- main/ec2.tf
+++ main/ec2.tf
@@ -1,7 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "web" {
-  ami           = "ami-031af0979071399d0"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   user_data_replace_on_change = true
   subnet_id                   = aws_subnet.public.id
